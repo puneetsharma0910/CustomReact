5bdc78419f57cece5d4b89d3b4bea05d6ab7d838
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-08-06 23:37:56 +0530

    Refactor script.js for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const mainContainer = document.querySelector('#root');
+// const mainContainer = document.querySelector('#root');
 
-const reactElement  = {
-    type : 'a',
-    props : {
-        href : "https://www.google.com",
-        target : "_blank",
-    },
-    children : "click here to google"
+// const reactElement  = {
+//     type : 'a',
+//     props : {
+//         href : "https://www.google.com",
+//         target : "_blank",
+//     },
+//     children : "click here to google"
+// }
+
+// renderIt(reactElement, mainContainer);
+
+// function renderIt(reactElement, mainContainer) {
+//     const domElement = document.createElement(reactElement.type);
+//     domElement.innerHTML = reactElement.children;
+
+//     for (const prop in reactElement.props) {
+//         domElement.setAttribute(prop, reactElement.props[prop]); // ✅ Fix
+//     }
+
+//     mainContainer.appendChild(domElement);
+// }
+
+
+
+let user  = {
+    name : "puneet",
+    company : "Microsoft, Amazon"
 }
 
-renderIt(reactElement, mainContainer);
+let copy = {};
+// for(let key in user){
+//     copy[key] = user[key]
+// }
+Object.assign(copy, user);
 
-function renderIt(reactElement, mainContainer) {
-    const domElement = document.createElement(reactElement.type);
-    domElement.innerHTML = reactElement.children;
-
-    for (const prop in reactElement.props) {
-        domElement.setAttribute(prop, reactElement.props[prop]); // ✅ Fix
-    }
-
-    mainContainer.appendChild(domElement);
-}
+console.log(copy)
