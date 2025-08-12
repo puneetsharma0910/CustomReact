7901ce25b53ac0a47e143012e276d3ce88ba0607
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-08-13 00:13:21 +0530

    updated modularity

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
-// const mainContainer = document.querySelector('#root');
+// // const mainContainer = document.querySelector('#root');
 
-// const reactElement  = {
-//     type : 'a',
-//     props : {
-//         href : "https://www.google.com",
-//         target : "_blank",
-//     },
-//     children : "click here to google"
+// // const reactElement  = {
+// //     type : 'a',
+// //     props : {
+// //         href : "https://www.google.com",
+// //         target : "_blank",
+// //     },
+// //     children : "click here to google"
+// // }
+
+// // renderIt(reactElement, mainContainer);
+
+// // function renderIt(reactElement, mainContainer) {
+// //     const domElement = document.createElement(reactElement.type);
+// //     domElement.innerHTML = reactElement.children;
+
+// //     for (const prop in reactElement.props) {
+// //         domElement.setAttribute(prop, reactElement.props[prop]); // ✅ Fix
+// //     }
+
+// //     mainContainer.appendChild(domElement);
+// // }
+
+
+
+// // let user  = {
+// //     name : "puneet",
+// //     company : "Microsoft, Amazon"
+// // }
+
+// // let copy = {};
+// // // for(let key in user){
+// // //     copy[key] = user[key]
+// // // }
+// // Object.assign(copy, user);
+
+// // console.log(copy)
+
+// let user = {
+//     name : "puneet sharma",
+//     belongsTo : "rajasthan"
 // }
 
-// renderIt(reactElement, mainContainer);
-
-// function renderIt(reactElement, mainContainer) {
-//     const domElement = document.createElement(reactElement.type);
-//     domElement.innerHTML = reactElement.children;
-
-//     for (const prop in reactElement.props) {
-//         domElement.setAttribute(prop, reactElement.props[prop]); // ✅ Fix
-//     }
-
-//     mainContainer.appendChild(domElement);
-// }
+// let copy = user;
+// //console.log(copy)
+// copy.name = "sharma puneet";
+// copy.belongsTo = "marwar, rajasthan"
+// console.log(user)
+// // for(let key in user){
 
 
 
-let user  = {
-    name : "puneet",
-    company : "Microsoft, Amazon"
-}
+// //     copy[key]  = user[key];
+// // }
+// // console.log(copy);
 
-let copy = {};
-// for(let key in user){
-//     copy[key] = user[key]
-// }
-Object.assign(copy, user);
 
-console.log(copy)
+let arr = [1,2,3,4];
+let arr2 = [100,101,105]
+console.log(arr.concat(arr2))
